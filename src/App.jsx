0bbe12c126fdf53eedd78149870309d1aa4cb027
jinvaramas <Jinvaramas@gamil.com>
2026-04-20 16:02:01 +0700

    CastleV2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-export default function App() {
-  return (
-    <div className="pb-80 py-10 gap-y-4 flex flex-col justify-center items-center min-h-screen bg-gray-800 text-white">
-      <p className="text-purple-300">
-        Message For JSD12
-        <span className="text-yellow-300">
-          {/* question or waiting for message */}
-        </span>
-      </p>
-      <textarea
-        defaultValue="banana"
-        onChange={() => {}}
-        className="bg-white text-black rounded px-2 py-1"
-        placeholder="Type your message here..."
-        />
+import { useState } from "react";
+import Castle from "./assets/components/Castle";
 
-      <p className="text-green-300">
-        Reply from Secret Room:
-        <span className="text-yellow-300">
-        {/* answer or waiting for a reply */}
-        </span>
-      </p>
-        <Castel />
-    </div>
-  );
+export default function App() {
+  // creating state variables
+  const [question, setQuestion] = useState("");
+  
+  const handleQuestion = (e) => {
+    console.log(e);
+    setQuestion(e.target.value);
+  };
+
+  return (
+    <div className="pb-80 py-10 gap-y-4 flex flex-col justify-center items-center min-h-screen bg-gray-800 text-white">
+      <p className="text-purple-300">
+        Message For JSD12: 
+        
+      </p>
+
+      <textarea
+        value={question}
+        onChange={handleQuestion}
+        className="bg-white text-black rounded px-4 py-2 w-64 focus:outline-none focus:ring-2 focus:ring-purple-500"
+        placeholder="Type your message here..."
+      />
+
+      <p className="text-green-300">
+        Reply from Secret Room: 
+        
+      </p>
+
+      <Castle />
+    </div>
+  );
 }
--- src/assets/components/Tower.jsx
+++ src/assets/components/Tower.jsx
@@ -1,8 +1,7 @@
-function Tower(){
-    return(
-        <div className="flex flex-col justify-center items-center pt-10 bg-orange-500 w-[90%]">
-            <h1>Tower</h1>
-
-        </div>
-    );
+export default function Tower() {
+  return (
+    <div className="flex flex-col justify-center items-center pt-10 bg-orange-500 w-[90%]">
+      <h1>Tower</h1>
+    </div>
+  );
 }
